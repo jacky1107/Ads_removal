4b5d31a9c6eb322d2dbd0b309dda64b91c0c8cf6
jacky1107 <jacky110711071107@gmail.com>
2021-03-14 16:46:10 +0800

    ^^

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 from skimage.metrics import peak_signal_noise_ratio, structural_similarity
 
 gt = {"test_5": [2800, 7870], "test_6": [2776, 8320], "Video_1": [939, 5438]}
+total = {"test_5": 9693, "test_6": [2776, 8320], "Video_1": 10250}
 
 normalized_std = np.array([0.229, 0.224, 0.225])
 normalized_mean = np.array([0.485, 0.456, 0.406])
+gamma = 1.5
 
 c = 0
-total = 10250
 features = []
 video_name = "test_5"
 cap = cv2.VideoCapture(f"videos/{video_name}.avi")
@@ -17,6 +18,9 @@ while True:
     res, img = cap.read()
     if not res:
         break
+
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    gamma_img = img ** (1 / gamma)
 
     mean, std = cv2.meanStdDev(img)
     std = np.mean(std)
@@ -29,7 +33,7 @@ while True:
     normal_std = np.mean(normal_std)
     normal_mean = np.mean(normal_mean)
     features.append([mean, std, normal_mean, normal_std])
-    print(f"\r{round((c / total) * 100, 2)}", end=" ")
+    print(f"\r{round((c / total[video_name]) * 100, 2)}", end=" ")
     c += 1
 
 features = np.array(features, dtype=object)
