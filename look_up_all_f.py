d44f3f7484dc9821a18f7e39591c37e29bd5105c
jacky1107 <jacky110711071107@gmail.com>
2021-03-15 11:02:03 +0000

    ^^

--- look_up_all_f.py
+++ look_up_all_f.py
@@ -48,7 +48,8 @@ def merge_gap_between_seg(segments, thres=15):
     return segments
 
 
-video_name = "test_6"
+save_feature = False
+video_name = "test_5"
 
 gt = {"test_5": [2800, 7870], "test_6": [2776, 8320], "Video_1": [939, 5438]}
 with open(f"{video_name}_features.npy", "rb") as f:
@@ -61,33 +62,43 @@ total_diff_features = len(diff_features)
 
 features_index_thres = {
     "test_5": [
-        (diff_features, 18, 750),
-        (diff_features, 19, 500),
+        (diff_features, 18, 750, 30),
+        (diff_features, 19, 500, 30),
     ],
-    "test_6": [],
-    "Video_1": [],
+    "test_6": [
+        (diff_features, 18, 650, 30),
+        (diff_features, 19, 280, 30),
+    ],
+    "Video_1": [
+        # (diff_features, 10, 0.3, 30),
+        (diff_features, 13, 330, 30),
+        (diff_features, 18, 600, 30),
+        # (diff_features, 19, 350, 30),
+    ],
 }
 
-# diff
-total_diff_features = len(diff_features)
-for i in range(len(diff_features[0])):
-    upper = total_diff_features - 1
-    x = np.arange(0, upper, upper / total_diff_features)
-    plt.plot(x, diff_features[:, i])
-    plt.scatter(gt[video_name][0], 0, c="#1f33b4")
-    plt.scatter(gt[video_name][1], 0, c="#1f33b4")
-    plt.savefig(f"features_local/{video_name}_{i}_diff.png")
-    plt.clf()
+if save_feature:
+    # diff
+    total_diff_features = len(diff_features)
+    for i in range(len(diff_features[0])):
+        upper = total_diff_features - 1
+        x = np.arange(0, upper, upper / total_diff_features)
+        plt.plot(x, diff_features[:, i])
+        plt.scatter(gt[video_name][0], 0, c="#1f33b4")
+        plt.scatter(gt[video_name][1], 0, c="#1f33b4")
+        plt.savefig(f"features_local/{video_name}_{i}_diff.png")
+        plt.clf()
 
-# origin
-for i in range(len(features[0])):
-    x = np.arange(total_imgs)
-    plt.plot(x, features[:, i])
-    plt.scatter(gt[video_name][0], 0, c="#1f33b4")
-    plt.scatter(gt[video_name][1], 0, c="#1f33b4")
-    plt.savefig(f"features_local/{video_name}_{i}.png")
-    plt.clf()
+    # origin
+    for i in range(len(features[0])):
+        x = np.arange(total_imgs)
+        plt.plot(x, features[:, i])
+        plt.scatter(gt[video_name][0], 0, c="#1f33b4")
+        plt.scatter(gt[video_name][1], 0, c="#1f33b4")
+        plt.savefig(f"features_local/{video_name}_{i}.png")
+        plt.clf()
 
+all_res = []
 for f_index_thres in features_index_thres[video_name]:
     index = f_index_thres[1]
     new = get_segmentation(
@@ -98,18 +109,81 @@ for f_index_thres in features_index_thres[video_name]:
     print(new)
     assert len(new) % 2 == 0
     new = new.reshape((len(new) // 2, 2))
-    new = merge_small_seg(new, 30)
-    new = merge_gap_between_seg(new, 30)
+    new = merge_small_seg(new, f_index_thres[3])
+    new = merge_gap_between_seg(new, f_index_thres[3])
     print(new)
 
-    upper = total_diff_features - 1
+    upper = total_diff_features
     x = np.arange(upper)
-    y = np.zeros(total_diff_features - 1)
+    y = np.zeros(total_diff_features)
     for i in range(len(new)):
-        y[new[i][0] : new[i][1]] = 50
+        y[new[i][0] : new[i][1]] = 1
+    all_res.append(y)
 
     plt.plot(x, y)
     plt.scatter(gt[video_name][0], 0, c="#1f33b4")
     plt.scatter(gt[video_name][1], 0, c="#1f33b4")
     plt.savefig(f"features_local/{video_name}_res_{index}.png")
     plt.clf()
+
+
+upper = total_diff_features
+x = np.arange(upper)
+y = np.zeros(total_diff_features)
+for i in range(upper):
+    unit = False
+    for res in all_res:
+        unit = unit or (res[i] != 0)
+    if unit:
+        y[i] = 1
+
+plt.plot(x, y)
+plt.scatter(gt[video_name][0], 0, c="#1f33b4")
+plt.scatter(gt[video_name][1], 0, c="#1f33b4")
+plt.savefig(f"features_local/{video_name}_res.png")
+plt.clf()
+y = np.append(y, 0)
+
+
+def cal_recall(confuse_matrix):
+    tp = confuse_matrix[0][0]
+    fn = confuse_matrix[1][0]
+    return tp / (tp + fn)
+
+
+def cal_precision(confuse_matrix):
+    tp = confuse_matrix[0][0]
+    fp = confuse_matrix[0][1]
+    return tp / (tp + fp)
+
+
+def evaluation(res):
+    gt_f, gt_s = gt[video_name]
+    tar = np.zeros(total_imgs)
+    tar[gt_f:gt_s] = 1
+
+    confuse_matrix = [[0, 0], [0, 0]]
+    for i in range(total_imgs):
+        if res[i] and (gt_f <= i and i < gt_s):
+            confuse_matrix[0][0] += 1
+        elif res[i] and (i < gt_f or gt_s >= i):
+            confuse_matrix[0][1] += 1
+        elif not res[i] and (gt_f <= i and i < gt_s):
+            confuse_matrix[1][0] += 1
+        elif not res[i] and (i < gt_f or gt_s >= i):
+            confuse_matrix[1][1] += 1
+
+    recall_rate = cal_recall(confuse_matrix)
+    precision_rate = cal_precision(confuse_matrix)
+
+    return recall_rate, precision_rate
+
+
+recall_rate, precision_rate = evaluation(y)
+
+
+print("================================")
+print(f"{video_name} result: ")
+print(f"Recall rate: {recall_rate}")
+print(f"Precision rate: {precision_rate}")
+print("================================")
